6806bfb4e92e663adec34e6a6043ad1b5d313a86
Dobby114 <2441697223@qq.com>
2025-03-31 21:47:38 +0800

    feat: 编辑问卷 右侧面板tab切换

---
+++ src/pages/question/Edit/PageSetting.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const PageSetting: FC = () => {
+  return <>页面设置</>;
+};
+
+export default PageSetting;
--- src/pages/question/Edit/RightPanel.tsx
+++ src/pages/question/Edit/RightPanel.tsx
@@ -1,14 +1,34 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { Tabs } from 'antd';
 import { ProfileOutlined, SettingOutlined } from '@ant-design/icons';
 import Props from './Props';
+import PageSetting from './PageSetting';
+import useGetComponentsData from '../../../hooks/useGetComponentsData';
 
+enum TAB_KEYS {
+  PROPS_KEY = 'props',
+  SETTING_KEY = 'pageSettings',
+}
 const RightPanel: FC = () => {
+  const { selectedId } = useGetComponentsData();
+  const [activeTab, setActiveTab] = useState(TAB_KEYS.PROPS_KEY);
+  useEffect(() => {
+    if (selectedId) {
+      setActiveTab(TAB_KEYS.PROPS_KEY);
+    } else {
+      setActiveTab(TAB_KEYS.SETTING_KEY);
+    }
+  }, [selectedId]);
   const tabItems = [
-    { key: 'props', label: '属性', icon: <ProfileOutlined />, children: <Props /> },
-    { key: 'pageSettings', label: '页面设置', icon: <SettingOutlined /> },
+    { key: TAB_KEYS.PROPS_KEY, label: '属性', icon: <ProfileOutlined />, children: <Props /> },
+    {
+      key: TAB_KEYS.SETTING_KEY,
+      label: '页面设置',
+      icon: <SettingOutlined />,
+      children: <PageSetting />,
+    },
   ];
-  return <Tabs items={tabItems}></Tabs>;
+  return <Tabs activeKey={activeTab} items={tabItems}></Tabs>;
 };
 
 export default RightPanel;
