4a2c3a28edd2bbb3495718ae3a8965873b0665b9
Dobby114 <2441697223@qq.com>
2025-04-07 21:56:28 +0800

    feat: 统计问卷 答卷列表显示

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -5,3 +5,4 @@ export const LIST_PAGE_SIZE_DEFAULT = 10;
 export const USERNAME_KEY = 'USERNAME';
 export const PASSWORD_KEY = 'PASSWORD';
 export const USER_TOKEN_KEY = 'USERTOKEN';
+export const STATE_PAGE_SIZE_DEFAULT = 10;
--- src/pages/question/Static/AnswerPage.tsx
+++ src/pages/question/Static/AnswerPage.tsx
@@ -2,7 +2,10 @@ import React, { FC, useState } from 'react';
 import { useRequest } from 'ahooks';
 import { getAnswerPage } from '../../../services/stat';
 import { useParams } from 'react-router-dom';
-import { Spin, Typography } from 'antd';
+import { Spin, Typography, Table, Pagination } from 'antd';
+import useGetComponentsData from '../../../hooks/useGetComponentsData';
+import { STATE_PAGE_SIZE_DEFAULT } from '../../../constant';
+import styles from './AnswerPage.module.scss';
 
 interface propsType {
   selectedComponentId: string;
@@ -10,14 +13,16 @@ interface propsType {
   setSelectedComponentType: (type: string) => void;
 }
 const AnswerPage: FC<propsType> = (props: propsType) => {
+  const { selectedComponentId, setSelectedComponentId, setSelectedComponentType } = props;
   const { id = '' } = useParams();
   const [total, setTotal] = useState(0);
   const [answerList, setAnswerList] = useState([]);
-  console.log(props, answerList);
+  const [pageSize, setPageSize] = useState(STATE_PAGE_SIZE_DEFAULT);
+  const [pageNo, setPageOn] = useState(1);
   const { Title } = Typography;
   const { loading } = useRequest(
     async () => {
-      const res = await getAnswerPage(id, { pageSize: 10, pageNo: 1 });
+      const res = await getAnswerPage(id, { pageSize, pageNo });
       return res;
     },
     {
@@ -26,17 +31,61 @@ const AnswerPage: FC<propsType> = (props: propsType) => {
         setAnswerList(list);
         setTotal(total);
       },
+      refreshDeps: [pageNo, pageSize],
     }
   );
+  const { componentsList } = useGetComponentsData();
+  const columns = componentsList.map(item => {
+    const { fe_id, type, title, props = {} as any } = item;
+    const tableTitle = props!.title || title;
+    return {
+      dataIndex: fe_id,
+      title: (
+        <div
+          style={{ cursor: 'pointer', color: fe_id === selectedComponentId ? '#0958d9' : '#000' }}
+          onClick={() => {
+            setSelectedComponentId(fe_id);
+            setSelectedComponentType(type);
+          }}
+        >
+          {tableTitle}
+        </div>
+      ),
+    };
+  });
+  const answerListWithId = answerList.map((item: any) => ({ ...item, key: item._id }));
+  const TableEl = (
+    <div>
+      <Table
+        columns={columns}
+        dataSource={answerListWithId}
+        pagination={false}
+        size="middle"
+      ></Table>
+      <div style={{ paddingTop: '16px' }}>
+        <Pagination
+          total={total}
+          align="center"
+          responsive={true}
+          onChange={(page, pageSize) => {
+            setPageOn(page);
+            setPageSize(pageSize);
+          }}
+          current={pageNo}
+        ></Pagination>
+      </div>
+    </div>
+  );
   return (
-    <>
+    <div className={styles.wrapper}>
       <Title level={3}>答卷数量：{!loading && total}</Title>
       {loading && (
         <div style={{ textAlign: 'center' }}>
           <Spin />
         </div>
       )}
-    </>
+      {!loading && TableEl}
+    </div>
   );
 };
 
